8161b644f009766645328acdc9e98b4366401392
0xJohnnyboy <lambert.theo@proton.me>
2025-09-26 12:58:32 +0200

    feat: two pointers

---
+++ two_pointers/two_pointers.go
@@ -0,0 +1,20 @@
+package two_pointers
+
+func TwoSum2(input []int, target int) ([2]int) {
+	left, right := 0, len(input)-1
+
+	for left < right {
+		sum := input[left] + input[right]
+		if sum == target {
+			return [2]int{left + 1, right + 1}
+		}
+
+		if sum < target {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return [2]int{}
+}
---
+++ two_pointers/two_pointers_test.go
@@ -0,0 +1,32 @@
+package two_pointers
+
+import (
+	"testing"
+)
+
+type TwoSum2TestCase struct {
+	Input  []int
+	Target int
+	Output [2]int
+}
+
+var twoSumTestCases = []TwoSum2TestCase{
+	{Input: []int{2, 7, 11, 15}, Target: 9, Output: [2]int{1, 2}},
+	{Input: []int{2, 3, 4}, Target: 6, Output: [2]int{1, 3}},
+	{Input: []int{3, 8, 15, 35, 43, 46, 48, 52}, Target: 87, Output: [2]int{4, 8}},
+}
+
+func TestTwoSum2(t *testing.T) {
+	for _, tc := range twoSumTestCases {
+		op := TwoSum2(tc.Input, tc.Target)
+
+		if tc.Output[0] != op[0] {
+			t.Errorf(`Output[0] %d doesn't match expected %d`, op[0], tc.Output[0])
+			return
+		}
+		if tc.Output[1] != op[1] {
+			t.Errorf(`Output[1] %d doesn't match expected %d`, op[1], tc.Output[1])
+			return
+		}
+	}
+}
